1084aea1b59c8ef0fb00b876fbbd26ea5d044b8b
Aurélien Hervé <mail@aurelien-herve.com>
2020-08-07 12:48:55 +0200

    simplify trello card creation

--- trello.go
+++ trello.go
@@ -22,40 +22,22 @@ func (card cardStruct) Export() error {
 	log.Println("exporting trello card")
 	token := os.Getenv("TRELLO_TOKEN")
 	key := os.Getenv("TRELLO_KEY")
-	//boardID := os.Getenv("TRELLO_BOARD_ID")
 	listID := os.Getenv("TRELLO_LIST_ID")
 
 	client := trello.NewClient(key, token)
 
 	// Create card (without attachment, unfortunately)
-	err := client.CreateCard(&trello.Card{
+	tCard := trello.Card{
 		Name:   card.Title,
 		Desc:   card.URL,
 		IDList: listID,
-	}, trello.Defaults())
+	}
+	err := client.CreateCard(&tCard, trello.Defaults())
 	if err != nil {
 		return err
 	}
-	log.Println("Card created. retrieving id to add attachment...")
-
-	// Retrieve card and add attachment. Tedious: create should return an ID !
-	list, err := client.GetList(listID, trello.Defaults())
-	if err != nil {
-		return err
-	}
-	cards, err := list.GetCards(trello.Defaults())
-	if err != nil {
-		return err
-	}
-	for _, c := range cards {
-		if c.Desc == card.URL {
-			log.Println("Card found. adding attachment...")
-			return c.AddURLAttachment(&trello.Attachment{
-				URL: card.Picture,
-			})
-		}
-	}
-
-	return nil
-
+	log.Println("Card created. adding attachment...")
+	return tCard.AddURLAttachment(&trello.Attachment{
+		URL: card.Picture,
+	})
 }
